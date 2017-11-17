ae5aa927e9e1d5e7ad888a4bbd2c35b8e9e0daf4
Charlie Monk <charlie@charliemonk.net>
2017-11-17 12:20:48 -0500

    Added additional lane detection functionality. The second algorithm uses solidity (a measure of convexity) to find lanes. (The last commit I forgot to stage basic_autopilot.py).

--- Software/basic_autopilot.py
+++ Software/basic_autopilot.py
@@ -14,26 +14,54 @@ def findRoadLines(image_path):
     # Morph closing on image
     kernel = np.ones((3,3), np.uint8)
     closed = cv2.morphologyEx(res, cv2.MORPH_CLOSE, kernel)
+
     # Lower and upper canny thresholds
-    lower_canny = 280
-    upper_canny = 400
+    lower_canny = 200
+    upper_canny = 300
+
     # Use canny to detect edges
     edges = cv2.Canny(closed, lower_canny, upper_canny)
     # Fit lines to the canny edges
-    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=20, maxLineGap=10)
+    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=40, maxLineGap=10)
+
     # If thresholding didn't work, use the original image
-    if(lines == None):
+    if (lines == None):
         edges = cv2.Canny(img, lower_canny, upper_canny)
-        lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=20, maxLineGap=10)
-    # Draw the lines
+        lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=40, maxLineGap=10)
+    for edge in edges:
+        img2, contour, hierarchy = cv2.findContours(edge, 1, 2)
+        cv2.imshow("Test", img2)
+        contour_area = 1#cv2.contourArea(contour)
+        if(contour_area > 30):
+            hull_area = cv2.contourArea(cv2.convexHull(contour))
+            solidity = float(contour_area)/hull_area
+            if(solidity>0.7):
+                x1, y1, x2, y2 = cv2.HoughLinesP(edge, 1, np.pi/180, 100, minLineLength=10, maxLineGap=10)
+                cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), thickness=10)
+    # Draw the lines (remove later)
     for line in lines:
         x1, y1, x2, y2 = line[0]
         cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), thickness=10)
 
+
+    # Draw rotated rectangles around the contours
+    img2, contours, hierarchy = cv2.findContours(edges, 1, 2)
+    for contour in contours:
+        contour_area = cv2.contourArea(contour)
+        if(contour_area > 30):
+            hull = cv2.convexHull(contour)
+            hull_area = cv2.contourArea(hull)
+            solidity = float(contour_area)/hull_area
+            if (solidity<0.8) and (solidity>0.05):
+                rect = cv2.minAreaRect(contour)
+                box = np.int0(cv2.boxPoints(rect))
+                cv2.drawContours(img, [box], 0, (0,0,255))
+                print(solidity)
+    cv2.imshow("edges", img2)
     return img
 
 # Run findRoadLines on a test image
-linesFound = findRoadLines("/Users/cbmonk/AnacondaProjects/SelfDrivingCar/Test/1.png")
+linesFound = findRoadLines("/Users/cbmonk/AnacondaProjects/SelfDrivingCar/Test/8.png")
 cv2.imshow("Lines detected", linesFound)
 
 # Close everything out when any key is pressed
